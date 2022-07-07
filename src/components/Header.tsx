f7b47081a536d7036ebe5684327220575ee7a74b
Walter <silvawalteralipio@gmail.com>
2022-07-07 10:06:19 -0300

    add hover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import  casa from "../assets/icons/Casa.svg";
+import casa from '../assets/icons/Casa.svg'
 import mensagens from "../assets/icons/Mensagens.svg";
 import logo from '../assets/icons/Logo-completo-02.png';
 
@@ -7,11 +7,15 @@ export default function Header(){
     <header className="p-7 h-24 mb-10">
       <nav className="flex items-center gap-10 p-7">
         <img src={logo} alt="Adopet logo" className="hidden" />
-        <a href="#">
-          <img src={casa} alt="Tela inicial" />
+        <a href="#" title="Página inicial" className="group">
+          <img src={casa} alt="Página inicial"
+            className='group-hover:animate-bounce hover:shadow-lg hover:shadow-black '
+          />
         </a>
-        <a href="#">
-          <img src={mensagens} alt="Enviar mensagem" />
+        <a href="#" title='Enviar mensagem' className="group">
+          <img src={mensagens} alt="Enviar mensagem" 
+             className='hover:animate-bounce hover:shadow-lg hover:shadow-black'
+          />
         </a>
       </nav>
     </header>
