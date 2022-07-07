6d11b5db9003ea1fdf10c3138cdb142e51ca4ff8
Walter <silvawalteralipio@gmail.com>
2022-07-07 10:55:28 -0300

    add image paws

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -6,6 +6,7 @@ export default function Registration(){
   return (
     <div className="bg-mobileRegister bg-contain bg-no-repeat h-screen">
       <Header />
+      <div className="bg-paws bg-no-repeat bg-cover w-36 h-52 absolute right-0 top-0"></div>
       <Main />
       <Footer/>
     </div>
