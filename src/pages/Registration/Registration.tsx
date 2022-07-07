fa003a94d8039b7ef11be07a4e5fb194c8d8de47
Walter <silvawalteralipio@gmail.com>
2022-07-07 10:08:29 -0300

    add page registration

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,5 +1,13 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Main from "./main/Main";
+
 export default function Registration(){
   return (
-    <div className="bg-mobile bg-contain bg-no-repeat h-screen"></div>
+    <div className="bg-mobileRegister bg-contain bg-no-repeat h-screen">
+      <Header />
+      <Main />
+      <Footer/>
+    </div>
   )
 }
