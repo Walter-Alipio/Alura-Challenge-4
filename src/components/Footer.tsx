19efc4b462573dc6709559ae6bc626298e4fc558
Walter <silvawalteralipio@gmail.com>
2022-07-07 15:41:12 -0300

    adjuste position

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
   return (
-    <footer className="w-screen h-24 bg-green-secondary bottom-0 fixed text-white text-center flex flex-col items-center justify-center">
+    <footer className="w-screen h-24 bg-green-secondary bottom-0 text-white text-center flex flex-col items-center justify-center">
       <span> 2022 - Projeto Alura  </span>
       <span> Desenvolvido por Walter Alípio </span>
 
