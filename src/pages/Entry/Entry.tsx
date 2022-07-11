a5747bfce80a2ee8b6831c5693c17565b3cddd0a
Walter <silvawalteralipio@gmail.com>
2022-07-11 11:14:09 -0300

    adjust Entry page

--- src/pages/Entry/Entry.tsx
+++ src/pages/Entry/Entry.tsx
@@ -1,13 +1,37 @@
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
-import Main from "./main/Main";
+import logo from '../../assets/icons/Logo-completo-02.png'
 
 export default function Entry(){
   return (
-    <div className="bg-[url(/src/assets/fundo/Forma1.svg),_url(/src/assets/fundo/Forma2.svg)] bg-[position:top_left,center_right]  bg-[size: contain,center]  bg-no-repeat h-screen bg-blue-logo flex flex-col">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <main className='h-full overflow-hidden'>
+
+      <section 
+        className='text-white bg-forma2 bg-right bg-no-repeat bg-blue-logo flex flex-col h-[calc(100vh-6rem)] pt-24 items-center'
+      >
+
+        <header className='m-auto'>
+          <img src={logo} alt="Logo adopet" className='w-44 mt-4 mb-4'/>
+        </header>
+        <main className='text-center m-4 flex flex-col items-center'>
+          <h1 className='font-semibold mb-4 text-2xl'>Boas-vinda!</h1>
+          <p className='mt-4 w-60'>Que tal mudar sua vida adotando seu novo melhor amigo? Vem com a gente!</p>
+        </main>
+        <nav className='flex flex-col w-80 '>
+          <a href="#" 
+            className='font-semibold mb-4 text-base text-center bg-coral-normal hover:bg-coral-hover rounded-md py-3 shadow-md transition-colors'
+            title='Logar'
+          >
+            Já tenho conta
+          </a>
+          <a href="#" 
+           className='font-semibold mb-4 text-base text-center bg-coral-normal hover:bg-coral-hover rounded-md py-3 px-4 shadow-md transition-colors'
+           title='Cadastrar'
+          >
+            Quero me cadastrar
+          </a>
+        </nav>
+       <div className='bg-catAndDog bg-contain bg-no-repeat w-48 h-56 mr-auto ml-16 -mb-4 mt-auto'></div>
+
+      </section>
+    </main>
   )
 }
