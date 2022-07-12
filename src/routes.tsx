79fbeaecd3c7ed723398cac6517808e8e077dd11
Walter <silvawalteralipio@gmail.com>
2022-07-11 21:58:07 -0300

    page register new login

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,5 @@
+
+import Registration from "./pages/Registration/Registration";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -8,7 +10,10 @@ export default function AppRoutes(){
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Entry />} />
+        <Route path="/">
+          <Route index  element={<Entry />} />
+          <Route path="register" element={<Registration />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
