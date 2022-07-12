187c0b0afb388809892f93c2142f49b697f90acc
Walter <silvawalteralipio@gmail.com>
2022-07-12 18:50:47 -0300

    add login screen

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import Button from "../../components/Button";
+import { AiFillEye, AiFillEyeInvisible} from 'react-icons/ai';
+import { seePassword } from "../../utils/seePassword";
+import logo from '../../assets/icons/Logo-completo.png';
+import { Link } from 'react-router-dom';
+
+
+export default function Login(){
+  
+  const [password,setPassword] = useState<'text' | 'password'>('password');
+  const [activeEyes,setActiveEyes] = useState(false);
+
+  return (
+    <main className='h-full overflow-hidden'>
+
+     <div className="bg-paws bg-no-repeat bg-cover w-36 h-52 absolute right-0 top-0"></div>
+
+      <section className="bg-forma2 bg-right bg-no-repeat flex flex-col h-[calc(100vh-6rem)] pt-24 items-center">
+
+       <header className='mt-16'>
+          <img src={logo} alt="Logo adopet" className='w-44 mt-4 mb-4'/>
+        </header>
+
+       <div className="text-center text-sm text-blue-logo w-80 my-10">
+        <p>Já tem conta? Faça seu login:</p>
+      </div>
+
+      <form action="#" className="flex flex-col items-center text-gray-hard gap-2 mb-4">
+              <label htmlFor="email" className="mt-2">E-mail</label>
+        <input type="text" id="email" 
+          className="pl-2 h-10 rounded-md shadow-md placeholder:text-center hover:border bg-gray-light placeholder:text-gray-medium text-gray-hard" 
+          placeholder="Escolha seu melhor email"/>
+
+
+          <label htmlFor="password" className="mt-2">Senha</label>
+        <div 
+          className=" h-10 rounded-md shadow-md hover:border bg-gray-light overflow-hidden relative flex items-center focus-within:outline focus-within:outline-orange-600 focus-within:outline-2">
+        
+          <input type={password} id="password" maxLength={20}
+            className="pl-2 h-full bg-inherit placeholder:text-center placeholder:text-gray-medium text-gray-hard" 
+            placeholder="Senha"/>
+            
+            <button 
+              className="absolute right-2"
+              onClick={event=>seePassword(
+                event,
+                setPassword,
+                setActiveEyes,
+                activeEyes
+                )}
+            >
+              { activeEyes ? <AiFillEye /> : <AiFillEyeInvisible />}
+            </button>
+        </div>
+
+        <Link to={''} className="text-coral-normal underline-offset-2 underline hover:no-underline text-xs">Esqueci minha senha.</Link>
+
+        <Button>Entrar</Button>
+      </form>
+    </section>
+   
+    </main>
+  )
+}
