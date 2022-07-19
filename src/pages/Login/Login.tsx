e7aee139ebb2dcb8a0855f0b6e5ae42b902d6ae7
Walter <silvawalteralipio@gmail.com>
2022-07-19 17:09:48 -0300

    validate inputs

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,12 +4,21 @@ import { AiFillEye, AiFillEyeInvisible} from 'react-icons/ai';
 import { seePassword } from "../../utils/seePassword";
 import logo from '../../assets/icons/Logo-completo.png';
 import { Link } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 
+interface IFormInput {
+  email: string;
+  password: string;
+}
 
 export default function Login(){
   
   const [password,setPassword] = useState<'text' | 'password'>('password');
   const [activeEyes,setActiveEyes] = useState(false);
+  const { register, handleSubmit, formState:{errors} } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = ( data ) => console.log(data);
 
   return (
     <main className='h-full overflow-hidden'>
@@ -28,20 +37,40 @@ export default function Login(){
         <p>Já tem conta? Faça seu login:</p>
       </div>
 
-      <form action="#" className="flex flex-col items-center text-gray-hard gap-2 mb-4 md:w-[21.5rem]">
-              <label htmlFor="email" className="mt-2">E-mail</label>
-        <input type="text" id="email" 
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center text-gray-hard gap-2 mb-4 md:w-[21.5rem]">
+        <label htmlFor="email" className="mt-2">E-mail</label>
+        <input type="text" {...register('email',
+          {
+            required: 'Campo e-mail deve ser preenchido.',
+            maxLength:20,
+            pattern: {
+              value: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+              message: 'Formato de email inválido.',
+            }
+          })}
+          id="email" 
+        
           className="pl-2 h-10 rounded-md shadow-md placeholder:text-center hover:border bg-gray-light placeholder:text-gray-medium text-gray-hard" 
-          placeholder="Escolha seu melhor email"/>
+          placeholder="Escolha seu melhor email"
+          />
+          <p className="text-red-600"><ErrorMessage errors = { errors } name='email' /></p>
+           
 
 
           <label htmlFor="password" className="mt-2">Senha</label>
         <div 
           className=" h-10 rounded-md shadow-md hover:border bg-gray-light overflow-hidden relative flex items-center focus-within:outline focus-within:outline-orange-600 focus-within:outline-2">
         
-          <input type={password} id="password" maxLength={20}
+          <input type={password} id="password"
+            {...register('password',
+              {
+                required: 'Campo senha deve ser preenchido.',
+
+              }
+              )}
             className="pl-2 h-full bg-inherit placeholder:text-center placeholder:text-gray-medium text-gray-hard" 
-            placeholder="Senha"/>
+            placeholder="Senha"
+            />
             
             <button 
               className="absolute right-2"
@@ -55,6 +84,8 @@ export default function Login(){
               { activeEyes ? <AiFillEye /> : <AiFillEyeInvisible />}
             </button>
         </div>
+        <p className="text-red-600"><ErrorMessage errors = { errors } name='password' /></p>
+         
 
         <Link to={''} className="text-coral-normal underline-offset-2 underline hover:no-underline text-xs">Esqueci minha senha.</Link>
 
